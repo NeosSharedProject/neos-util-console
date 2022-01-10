3de9b163ea96fb3632462275a5c83ac92b8a19f9
rhenium <rhenium750@gmail.com>
2022-01-11 00:08:49 +0900

    順番を変更

--- pages/inventory/v1/viewer/[ownerId]/[...slug].tsx
+++ pages/inventory/v1/viewer/[ownerId]/[...slug].tsx
@@ -33,6 +33,11 @@ const Index = () => {
     directory: "yellow",
     link: "orange",
   };
+  const orderMap = {
+    object: 1,
+    directory: 0,
+    link: 0,
+  };
 
   const { data } = useSWR<
     [{ id: string; recordType: string; name: string; assetUri: string }]
@@ -44,55 +49,61 @@ const Index = () => {
   );
   return (
     <GridStyle>
-      {_.map(data, ({ recordType, name, assetUri }) => {
-        const assetId = _.last(_.split(assetUri, "/"));
-        console.log(assetUri);
-        return (
-          <ItemStyle backColor={_.get(colorMap, recordType, "skyblue")}>
-            <div className="title">
-              {recordType === "object" && (
-                <button
-                  onClick={async () => {
-                    console.log("he");
-                    const response = await fetch(
-                      `https://decompress.kokoa.dev/?id=${_.first(
-                        _.split(assetId, ".")
-                      )}`
-                    );
-                    const data = await response.text();
-                    console.log("aa", data);
-                    const blob = new Blob([data]);
-                    const element = document.createElement("a");
-                    element.href = window.URL.createObjectURL(blob);
-                    element.setAttribute("download", `${name}.json`);
-                    document.body.appendChild(element);
-                    element.click();
-                    element.remove();
-                  }}
-                >
-                  {_.slice(name, 0, 256)}
-                </button>
-                // <a
-                //   href={`https://decompress.kokoa.dev/?id=${_.first(
-                //     _.split(assetId, ".")
-                //   )}`}
-                //   download
-                // >
-                //   {_.slice(name, 0, 256)}
-                // </a>
-              )}
-              {recordType === "link" && (
-                <a href={`/inventory/v1/link/${ownerId}/${assetId}`}>
-                  {_.slice(name, 0, 256)}
-                </a>
-              )}
-              {recordType === "directory" && (
-                <a href={`${currentDir}/${name}`}>{_.slice(name, 0, 256)}</a>
-              )}
-            </div>
-          </ItemStyle>
-        );
-      })}
+      {_(data)
+        .sortBy(({ recordType, name }) => [
+          _.get(orderMap, recordType, -1),
+          name,
+        ])
+        .map(({ recordType, name, assetUri }) => {
+          const assetId = _.last(_.split(assetUri, "/"));
+          console.log(assetUri);
+          return (
+            <ItemStyle backColor={_.get(colorMap, recordType, "skyblue")}>
+              <div className="title">
+                {recordType === "object" && (
+                  <button
+                    onClick={async () => {
+                      console.log("he");
+                      const response = await fetch(
+                        `https://decompress.kokoa.dev/?id=${_.first(
+                          _.split(assetId, ".")
+                        )}`
+                      );
+                      const data = await response.text();
+                      console.log("aa", data);
+                      const blob = new Blob([data]);
+                      const element = document.createElement("a");
+                      element.href = window.URL.createObjectURL(blob);
+                      element.setAttribute("download", `${name}.json`);
+                      document.body.appendChild(element);
+                      element.click();
+                      element.remove();
+                    }}
+                  >
+                    {_.slice(name, 0, 256)}
+                  </button>
+                  // <a
+                  //   href={`https://decompress.kokoa.dev/?id=${_.first(
+                  //     _.split(assetId, ".")
+                  //   )}`}
+                  //   download
+                  // >
+                  //   {_.slice(name, 0, 256)}
+                  // </a>
+                )}
+                {recordType === "link" && (
+                  <a href={`/inventory/v1/link/${ownerId}/${assetId}`}>
+                    {_.slice(name, 0, 256)}
+                  </a>
+                )}
+                {recordType === "directory" && (
+                  <a href={`${currentDir}/${name}`}>{_.slice(name, 0, 256)}</a>
+                )}
+              </div>
+            </ItemStyle>
+          );
+        })
+        .value()}
     </GridStyle>
   );
 };
