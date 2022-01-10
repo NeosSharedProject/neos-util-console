66f5b23cb9cb81226bebdba62d157cb91a1774aa
rhenium <rhenium750@gmail.com>
2022-01-10 23:59:10 +0900

    トップページのサンプルを追加

--- pages/inventory/v1/top.tsx
+++ pages/inventory/v1/top.tsx
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import _ from "lodash";
+import styled from "styled-components";
+
+const TextFiledStyle = styled.input`
+  width: 400px;
+`;
 
 const Index = () => {
   const router = useRouter();
@@ -9,7 +14,22 @@ const Index = () => {
   return (
     <div>
       <div>
-        <a href="/inventory/v1/link/U-rhenium/R-32338edd-bd95-4e38-a1ea-a0f24e4ecda6">
+        <a href="/inventory/v1/viewer/G-Neos/Inventory/Essential Tools">
+          Essential Tools
+        </a>
+      </div>
+      <div>
+        <a href="/inventory/v1/viewer/G-Neos/Inventory/Neos Essentials">
+          Neos Essentials
+        </a>
+      </div>
+      <div>
+        <a href="/inventory/v1/viewer/G-Shared-Project-rheni/Inventory/JP Publics">
+          JP Publics
+        </a>
+      </div>
+      <div>
+        <a href="/inventory/v1/viewer/U-rhenium/Inventory/rhenium Public">
           rhenium Public
         </a>
       </div>
@@ -22,7 +42,11 @@ const Index = () => {
             router.replace(`/inventory/v1/link/${ownerId}/${recordId}`);
           })}
         >
-          <input type="text" {...register("link")}></input>
+          <TextFiledStyle
+            type="text"
+            {...register("link")}
+            placeholder="neosrec:///U-xxxx/R-xxxx"
+          ></TextFiledStyle>
           <button type="submit">移動</button>
         </form>
       </div>
